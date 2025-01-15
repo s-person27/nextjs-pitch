6ec249005bc855a4a72491ebf22726334cb45a44
Danil Nikolaichuk <s.person2734@gmail.com>
2025-01-15 19:17:25 +0200

    feat: next auth & navbar

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,48 @@
+import { auth, signOut, signIn } from "@/auth";
+import Image from "next/image";
+import Link from "next/link";
+
+const Navbar = async () => {
+  const session = await auth();
+
+  return (
+    <header className="py-3 px-5 bg-white font-work-sans shadow-sm">
+      <nav className="flex justify-between items-center">
+        <Link href="/">
+          <Image src="/logo.png" alt="logo" width={144} height={30} />
+        </Link>
+        <div className="flex items-center text-black gap-5">
+          {session && session.user ? (
+            <>
+              <Link href="/startup/create">
+                <span>Create</span>
+              </Link>
+              <form
+                action={async () => {
+                  "use server";
+                  await signOut({ redirectTo: "/" });
+                }}
+              >
+                <button type="submit">Logout</button>
+              </form>
+              <Link href={`/user/${session?.user?.id}`}>
+                <span>{session?.user?.name}</span>
+              </Link>
+            </>
+          ) : (
+            <form
+              action={async () => {
+                "use server";
+                await signIn("github");
+              }}
+            >
+              <button type="submit">Login</button>
+            </form>
+          )}
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
