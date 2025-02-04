7d0e074b89dd13d47172873a981d77971cbc5a8d
Danil Nikolaichuk <s.person2734@gmail.com>
2025-02-04 19:02:01 +0200

    Added startup details page

--- sanity/schemaTypes/author.ts
+++ sanity/schemaTypes/author.ts
@@ -9,6 +9,10 @@ export const author = defineType({
   fields: [
     defineField({ name: "id", type: "number" }),
     defineField({ name: "name", type: "string" }),
+    defineField({
+      name: "username",
+      type: "string",
+    }),
     defineField({ name: "email", type: "string" }),
     defineField({ name: "image", type: "url" }),
     defineField({ name: "bio", type: "text" }),
